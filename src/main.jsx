4f4f1a765657e1791240be31d20d9d2c76ec7871
joo1e <urams79@gmail.com>
2024-02-22 22:41:42 +0900

    1차 초기세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import GlobalFontStyle from './assets/fonts/GlobalFontsStyle.jsx';
+import { GlobalStyled } from './styles/Globalstyled.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div>
+        <GlobalStyled />
+        <GlobalFontStyle />
+        <App />
+    </div>,
+);
