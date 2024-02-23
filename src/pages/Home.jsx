461c051652250682d2d0fa71ac893abdd703ad67
이준구 <leejungoo1396@gmail.com>
2024-02-23 15:55:48 +0900

    3. 초기설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import useInput from '../hooks/useInput';
 
 function Home() {
-    return <div>Home</div>;
+  const [name, onChangNameHandler] = useInput();
+  const [password, onChangePasswordHandler] = useInput();
+
+  return (
+    <>
+      <input type="text" value={name} onChange={onChangNameHandler}></input>
+      <input type="text" value={password} onChange={onChangePasswordHandler}></input>
+      <div>Home</div>
+    </>
+  );
 }
 
 export default Home;
