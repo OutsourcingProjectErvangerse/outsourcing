96ab90e7ad0525ab1c24240ee281b3773cb82b94
ugyobppang <yeonguk0517@gmail.com>
2024-02-27 23:10:58 +0900

    refactor : 검색 기능 코드 리팩토링

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,57 +1,91 @@
-import React from 'react';
-import SearchIcon from '../assets/search.svg';
-import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-// import { addTodo } from '../shared/store/modules/test';
+import { useState } from 'react';
+import { PROVINCES, DISTRICTS, PLACES } from '../shared/cityData';
+import { useDispatch } from 'react-redux';
 import { changeSearchText } from '../shared/store/modules/search';
 
 function Search() {
-  const text = useSelector((state) => state.search);
-  console.log(text);
+  const [seletedProvince, setSeletedProvince] = useState('');
+  const [selectedDistrict, setSelectedDistrict] = useState('');
 
   const dispatch = useDispatch();
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    const search = event.target.search.value;
+  // 시/도 선택
+  const handleSeletedProvinces = (e) => {
+    setSeletedProvince(e.target.value);
+  };
 
-    if (!search) {
-      alert('장소 및 주소를 입력해주세요.');
+  // 시/군/구 선택
+  const handleSeletedDistrict = (e) => {
+    setSelectedDistrict(e.target.value);
+  };
+
+  // 카테고리 선택
+  const handleCategoryButton = (attractions) => {
+    if (!seletedProvince || !selectedDistrict) {
+      alert('모든 항목을 선택해주세요.');
       return;
     }
 
-    dispatch(changeSearchText(search));
-    event.target.reset();
+    dispatch(changeSearchText(`${seletedProvince} ${selectedDistrict} ${attractions}`));
+
+    setSeletedProvince('');
+    setSelectedDistrict('');
   };
 
+  let districtToggle = (
+    <select>
+      <option value="" hidden>
+        시/군/구 선택
+      </option>
+      <option value="">시/도를 먼저 선택해주세요.</option>
+    </select>
+  );
+
+  if (seletedProvince) {
+    districtToggle = (
+      <select value={selectedDistrict} onChange={handleSeletedDistrict}>
+        <option value="" hidden>
+          시/군/구 선택
+        </option>
+        {DISTRICTS[seletedProvince].map((district) => {
+          return (
+            <option key={district} value={district}>
+              {district}
+            </option>
+          );
+        })}
+      </select>
+    );
+  }
+
   return (
-    <SearchForm onSubmit={handleSearchSubmit}>
-      <input type="text" placeholder="장소, 주소 검색" name="search" />
-      <button>
-        <img src={SearchIcon} alt="Search" />
-      </button>
-    </SearchForm>
+    <>
+      <div>
+        <select value={seletedProvince} onChange={handleSeletedProvinces}>
+          <option value="" hidden>
+            시/도 선택
+          </option>
+          {PROVINCES.map((provinces) => {
+            return (
+              <option key={provinces.name} value={provinces.name}>
+                {provinces.name}
+              </option>
+            );
+          })}
+        </select>
+        {districtToggle}
+      </div>
+      <div>
+        {PLACES.map((place) => {
+          return (
+            <button key={place} onClick={() => handleCategoryButton(place)}>
+              {place}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
 export default Search;
-
-const SearchForm = styled.form`
-  display: flex;
-  flex-direction: row;
-  & input {
-    width: 300px;
-    height: 40px;
-    padding: 10px;
-  }
-
-  & button {
-    width: 40px;
-    height: 40px;
-    padding: 0;
-  }
-  & button > img {
-    width: 30px;
-    height: 35px;
-  }
-`;
